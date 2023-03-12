f866cea736e9830ef9fbfb8e9ac994541e0056f8
cksdms977 <123286868+cksdms977@users.noreply.github.com>
2023-03-12 23:19:12 +0900

    Add files via upload

---
+++ 04반복문/src/com/repeate/run/Main.java
@@ -0,0 +1,14 @@
+package com.repeate.run;
+
+import com.repeate.controller.RepeateController;
+
+public class Main {
+	public static void main(String[] args) {
+		new RepeateController().basicRepeat();
+		
+		
+		
+		
+		
+	}
+}
